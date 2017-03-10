63e75e3ed73cfd27d014e5cecaf43baebe237259
Nikita Ermolenko <gnod94@gmail.com>
2017-03-10 20:27:13 +0600

    Add Workspace with example project

---
+++ SnapsureSDKExample/SnapsureSDK-iOS/SnapsureSDK-Example-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapsureSDK-Example-iOS
+//
+//  Created by Nikita Ermolenko on 10/03/2017.
+//  Copyright © 2017 Snapsure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
