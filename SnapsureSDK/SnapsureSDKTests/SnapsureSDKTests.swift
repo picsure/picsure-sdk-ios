33067edc4f19a5971a53e6ba6381cb53e471aab4
Nikita Ermolenko <otbivnoe@Nikitas-Mini.Dlink>
2017-03-10 19:04:04 +0600

    Generate empty framework

---
+++ SnapsureSDK/SnapsureSDKTests/SnapsureSDKTests.swift
@@ -0,0 +1,36 @@
+//
+//  SnapsureSDKTests.swift
+//  SnapsureSDKTests
+//
+//  Created by Nikita Ermolenko on 10/03/2017.
+//  Copyright © 2017 Snapsure. All rights reserved.
+//
+
+import XCTest
+@testable import SnapsureSDK
+
+class SnapsureSDKTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
