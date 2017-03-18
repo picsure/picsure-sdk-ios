b661d79cee4226c040eed2b82f0d9b8659f5a770
Artem Novichkov <novichkoff93@gmail.com>
2017-03-18 11:25:00 +0600

    Add basic example with camera using

--- SnapsureSDKExample/SnapsureSDK-iOS/SnapsureSDK-Example-iOS/ViewController.swift
+++ SnapsureSDKExample/SnapsureSDK-iOS/SnapsureSDK-Example-iOS/ViewController.swift
@@ -11,10 +11,51 @@ import SnapsureSDK
 
 class ViewController: UIViewController {
     
+    private lazy var takePhotoButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("Take a photo", for: .normal)
+        button.addTarget(self, action: #selector(takePhotoButtonAction), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(takePhotoButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        takePhotoButton.frame = CGRect(x: 0, y: 0, width: 200, height: 30)
+        takePhotoButton.center = view.center
+    }
+    
+    //MARK: - Snapsure SDK
+    
+    fileprivate func upload(photo: UIImage) {
+        print("Processing...")
         SnapsureSDK.uploadPhoto(#imageLiteral(resourceName: "saw")) { result in
             debugPrint(result)
         }
+    }
+    
+    //MARK: - Actions
+    
+    @objc private func takePhotoButtonAction() {
+        let pickerController = UIImagePickerController()
+        pickerController.sourceType = .camera
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
+    }
+}
+
+//MARK: - UIImagePickerControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            upload(photo: image)
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
 }
